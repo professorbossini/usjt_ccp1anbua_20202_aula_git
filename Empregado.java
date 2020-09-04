ad3ca83c928c2817e6f798428640ec34eabce5e2
Rodrigo Bossini <professorbossini@gmail.com>
2020-09-03 21:25:39 -0300

    Primeiro teste concluido com sucesso

--- Empregado.java
+++ Empregado.java
@@ -39,4 +39,22 @@ public class Empregado{
   public void setNome(String nome) {
     this.nome = nome;
   }
+  public double getBonus() {
+    return bonus;
+  }
+  public double getComissao() {
+    return comissao;
+  }
+  public double getSalario() {
+    return salario;
+  }
+  public void setBonus(double bonus) {
+    this.bonus = bonus;
+  }
+  public void setComissao(double comissao) {
+    this.comissao = comissao;
+  }
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
